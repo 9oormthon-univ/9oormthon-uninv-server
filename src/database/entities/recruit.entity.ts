78f5a35cd41d37fbcf824e302186b40d41fde465
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-09-02 22:41:43 +0900

    feat: add Recruit Entity

---
+++ src/database/entities/recruit.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Recruit {
+  @PrimaryGeneratedColumn({ name: 'recruit_id' })
+  id: number;
+
+  @Column({ name: 'type' })
+  type: string;
+
+  @Column({ name: 'title' }) // ex) 유니브 대표 모집, 미르미 모집, etc.
+  title: string;
+
+  @Column({
+    name: 'start_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  startAt: Date;
+
+  @Column({
+    name: 'end_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  endAt: Date;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+}
