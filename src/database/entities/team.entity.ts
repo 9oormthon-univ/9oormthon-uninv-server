639fe2cb010d25f5e3be94d499753b8d959579ce
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-05 10:58:11 +0900

    feat/#21: team 도메인 수정 반영

--- src/database/entities/team.entity.ts
+++ src/database/entities/team.entity.ts
@@ -7,24 +7,36 @@ import {
 } from 'typeorm';
 import { Idea } from './idea.entity';
 
-@Entity()
+@Entity('teams')
 export class Team {
-  @PrimaryGeneratedColumn({ name: 'team_id' })
+  /* ----------------------------- */
+  /* ------- Default Column ------ */
+  /* ----------------------------- */
+  @PrimaryGeneratedColumn({ name: ' id' })
   id: number;
 
+  /* ----------------------------- */
+  /* ---- Information Column ----- */
+  /* ----------------------------- */
+  @Column({ name: 'name', nullable: true })
+  name: string;
+
+  @Column({ name: 'generation', nullable: false })
+  generation: number;
+
+  @Column({ name: 'number', nullable: true })
+  number: number;
+
+  /* ----------------------------- */
+  /* ----- One To One Column ----- */
+  /* ----------------------------- */
   @ManyToOne(() => Idea)
   @JoinColumn({ name: 'idea_id' })
   idea: Idea;
 
-  @Column({ name: 'team_name' })
-  teamName: string;
-
-  @Column({ name: 'service_name' })
-  serviceName: string;
-
-  @Column({ name: 'service_content' })
-  serviceContent: string;
-
+  /* ----------------------------- */
+  /* ---- TimeStamp Column ------- */
+  /* ----------------------------- */
   @Column({
     name: 'created_at',
     type: 'timestamp',
