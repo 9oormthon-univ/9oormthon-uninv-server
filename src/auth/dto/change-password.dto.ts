7a29de62560aacc96fa621128d3aa8a519680c25
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-05 14:59:18 +0900

    feat/#16: 비밀번호 변경 시, '현재 비밀번호'에 대한 유효성 검사 제거

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -9,10 +9,6 @@ export class ChangePasswordDto {
   })
   @IsNotEmpty({ message: 'password는 null이 될 수 없습니다.' })
   @IsString()
-  @Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{10,20}$/, {
-    message:
-      '비밀번호는 소문자 1개 이상, 숫자 1개 이상, 특수문자(!, @, #, %, $) 1개 이상으로 구성된 10~20자리 비밀번호로 입력해주세요.',
-  })
   @Expose({ name: 'current_password' })
   currentPassword: string;
   @ApiProperty({
