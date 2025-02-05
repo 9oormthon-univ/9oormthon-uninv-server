40d3f1044ef4ff3b49486652f6e6e8691385405a
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-05 10:59:39 +0900

    feat/#21: 도메인 수정에 따른 auth 관련 로직 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { UserModule } from '../user/user.module';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' }, 
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   providers: [AuthService, JwtStrategy, JwtAuthGuard],
