95195b6df70c11f586e23d975bedd9275bfc5075
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-06 14:39:14 +0900

    chore/#38: 유효성 검사 수정

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,14 +2,14 @@ import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
-export class AuthSignUpDto {
+export class LoginDto {
   @ApiProperty({
     description: '시리얼 ID',
     example: 'example@example.com',
   })
   @IsNotEmpty({ message: 'serial_id는 null이 될 수 없습니다.' })
   @IsString()
-  @Length(6, 254, { message: '시리얼 ID는 6~254자리로 입력해주세요.' })
+  @Length(6, 20, { message: '시리얼 ID는 6~20자리로 입력해주세요.' })
   @Matches(/^[a-z0-9]{6,20}$/, {
     message: '소문자 또는 숫자로 이루어진 6자 이상 20자 미만이어야 합니다.',
   })
@@ -20,12 +20,8 @@ export class AuthSignUpDto {
     description: '비밀번호',
     example: '1234567890a!',
   })
-  @IsNotEmpty({ message: 'password는 null이 될 수 없습니다.' })
+  @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
   @IsString()
-  @Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{10,20}$/, {
-    message:
-      '비밀번호는 소문자 1개 이상, 숫자 1개 이상, 특수문자(!, @, #, %, $) 1개 이상으로 구성된 10~20자리 비밀번호로 입력해주세요.',
-  })
   @Expose({ name: 'password' })
   password: string;
 }
