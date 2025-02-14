318bc2ca2c96e45c4dc45fce7276625254e8c176
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-14 13:55:49 +0900

    fix/#87: 아이디어 상세조회 capacity 형식 수정

--- src/idea/application/service/read-idea-detail.service.ts
+++ src/idea/application/service/read-idea-detail.service.ts
@@ -7,6 +7,8 @@ import { UserRepositoryImpl } from '../../../user/repository/user.repository.imp
 import { DataSource } from 'typeorm';
 import { ReadIdeaDetailResponseDto } from '../dto/response/read-idea-detail.response.dto';
 import { ReadMyIdeaDetailResponseDto } from '../dto/response/read-my-idea-detail.response.dto';
+import { CommonException } from '../../../core/exceptions/common.exception';
+import { ErrorCode } from '../../../core/exceptions/error-code';
 
 @Injectable()
 @UseFilters(HttpExceptionFilter)
@@ -22,10 +24,19 @@ export class ReadIdeaDetailService implements ReadIdeaDetailUseCase {
     return this.dataSource.transaction(async (manager) => {
 
       const user = await this.userRepository.findByIdWithUniv(userId, manager);
+      if(!user) {
+        throw new CommonException(ErrorCode.NOT_FOUND_RESOURCE);
+      }
 
       const { idea, isBookmarked, isActive } = await this.ideaRepository.findIdeaDetail(ideaId, userId, manager);
+      if(!idea) {
+        throw new CommonException(ErrorCode.NOT_FOUND_RESOURCE);
+      }
 
       const team = await this.teamRepository.findByIdeaWithIdeaAndMembers(idea, manager);
+      if(!team) {
+        throw new CommonException(ErrorCode.NOT_FOUND_RESOURCE);
+      }
 
       return ReadMyIdeaDetailResponseDto.of(user, idea, team, isActive, isBookmarked);
     });
