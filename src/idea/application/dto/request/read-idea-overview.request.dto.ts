73c592aa4a091fa12651954ceb609e344a5fdf09
장동겸 <jangelliot0404@dgu.ac.kr>
2025-02-17 14:01:19 +0900

    🔨 Fix - 아이디어 요약 조회 RequestDto에 Expose 누락 버그 수정 (#95)

--- src/idea/application/dto/request/read-idea-overview.request.dto.ts
+++ src/idea/application/dto/request/read-idea-overview.request.dto.ts
@@ -1,6 +1,6 @@
 // src/idea/dto/query/read-idea-overview-query.dto.ts
 import { IsNotEmpty, IsNumber, IsOptional, IsBoolean } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 
 export class ReadIdeaOverviewQueryDto {
   @IsNotEmpty({ message: 'page는 필수 값입니다.' })
@@ -22,17 +22,20 @@ export class ReadIdeaOverviewQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsNumber({}, { message: 'subject-id는 숫자여야 합니다.' })
+  @Expose({ name: 'subject-id' })
   subjectId?: number;
 
   // isActive는 선택사항 (모집중/완료 필터)
   @IsOptional()
   @Type(() => Boolean)
   @IsBoolean({ message: 'is-active는 boolean 값이어야 합니다.' })
+  @Expose({ name: 'is-active' })
   isActive?: boolean;
 
   // isBookmarked는 선택사항 (내 찜 필터)
   @IsOptional()
   @Type(() => Boolean)
   @IsBoolean({ message: 'is-bookmarked는 boolean 값이어야 합니다.' })
+  @Expose({ name: 'is-bookmarked' })
   isBookmarked?: boolean;
 }
