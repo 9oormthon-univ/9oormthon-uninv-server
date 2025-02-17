cd1e0e9ebb1494a614de5cde40a068abb3da5f30
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-17 19:21:51 +0900

    fix/#104: 아이디어 지원 예외처리

--- src/system-setting/domain/system-setting.model.ts
+++ src/system-setting/domain/system-setting.model.ts
@@ -23,7 +23,28 @@ export class SystemSettingModel {
     public readonly updatedAt: Date
   ) {}
 
-  public validateIdeaApplyPeriod(): void {
+  public getWhichPeriod(): string {
+    const now = new Date();
+    if (this.ideaSubmissionStart <= now && now <= this.ideaSubmissionEnd) {
+      return 'ideaSubmission';
+    } else if (this.phase1TeamBuildingStart <= now && now <= this.phase1TeamBuildingEnd) {
+      return 'phase1TeamBuilding';
+    } else if (this.phase1ConfirmationStart <= now && now <= this.phase1ConfirmationEnd) {
+      return 'phase1Confirmation';
+    } else if (this.phase2TeamBuildingStart <= now && now <= this.phase2TeamBuildingEnd) {
+      return 'phase2TeamBuilding';
+    } else if (this.phase2ConfirmationStart <= now && now <= this.phase2ConfirmationEnd) {
+      return 'phase2Confirmation';
+    } else if (this.phase3TeamBuildingStart <= now && now <= this.phase3TeamBuildingEnd) {
+      return 'phase3TeamBuilding';
+    } else if (this.phase3ConfirmationStart <= now && now <= this.phase3ConfirmationEnd) {
+      return 'phase3Confirmation';
+    } else {
+      return 'none';
+    }
+  }
+
+  public validateIdeaApplyPeriod(phase: number): void {
     const now = new Date();
     if (
       !(
@@ -34,7 +55,10 @@ export class SystemSettingModel {
     ) {
       throw new CommonException(ErrorCode.NOT_IDEA_APPLY_PERIOD_ERROR);
     }
-
+    // phase 와 현재 기간 불일치시 예외처리
+    if (this.getWhichPeriod() !== `phase${phase}TeamBuilding`) {
+      throw new CommonException(ErrorCode.NOT_IDEA_APPLY_PERIOD_ERROR);
+    }
   }
 }
 
