08a22236c8bfa6a76b9d6732e438b1bc593499b8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-20 14:48:26 +0900

    fix/#123: response 반환값 수정

--- src/auth/application/dto/response/read-auth-brief.response.dto.ts
+++ src/auth/application/dto/response/read-auth-brief.response.dto.ts
@@ -2,16 +2,16 @@ import { ESecurityRole } from '../../../../core/enums/security-role.enum';
 
 export class ReadAuthBriefResponseDto {
   role: ESecurityRole;
-  profile_img_url: string;
+  img_url: string;
   is_provider: boolean;
 
-  constructor(role: ESecurityRole, profileImgUrl: string, isProvider: boolean) {
+  constructor(role: ESecurityRole, imgUrl: string, isProvider: boolean) {
     this.role = role;
-    this.profile_img_url = profileImgUrl;
+    this.img_url = imgUrl;
     this.is_provider = isProvider;
   }
 
-  static of (role: ESecurityRole, profileImgUrl: string, isProvider: boolean): ReadAuthBriefResponseDto {
-    return new ReadAuthBriefResponseDto(role, profileImgUrl, isProvider);
+  static of (role: ESecurityRole, imgUrl: string, isProvider: boolean): ReadAuthBriefResponseDto {
+    return new ReadAuthBriefResponseDto(role, imgUrl, isProvider);
   }
 }
