9644f095b91e105b9aa350c17480ac02345aa83c
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-02-27 18:45:03 +0900

    chore/#175: main에 전역으로 crypto 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './core/filters/http-exception.filter';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ValidationException } from './core/exceptions/common.exception';
+import * as crypto from 'crypto';
+globalThis.crypto = crypto.webcrypto as unknown as Crypto;
 
 dotenv.config();
 async function bootstrap() {
