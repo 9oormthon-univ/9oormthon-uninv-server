c83095b09974f29e23160d4e9f0673160364b996
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-06-23 20:41:06 +0900

    fix/#296: 팀 확정하기 서버 에러 수정

--- src/team/repository/team.repository.ts
+++ src/team/repository/team.repository.ts
@@ -49,7 +49,7 @@ export class TeamRepository {
 
     const team = await repo.findOne({
       where: { id: teamIdResult.id },
-      relations: ['members', 'members.team', 'members.user', 'members.user.univ'],
+      relations: ['members', 'members.team', 'members.user', 'members.user.univ', 'idea'],
     });
     return team ? TeamMapper.toDomain(team, { skipIdea: true, skipMembers: false }) : undefined;
   }
