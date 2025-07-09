90f00d3e97306cefd55173d232a6b116c26c0e47
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-07-09 23:41:00 +0900

    fix/#331: 현재 수락한 지원자를 고려하지 않고 팀원 파트 수 변경할 수 있던 로직오류 수정

--- src/idea/application/service/update-idea.service.ts
+++ src/idea/application/service/update-idea.service.ts
@@ -8,20 +8,34 @@ import { CommonException } from '../../../core/exceptions/common.exception';
 import { ErrorCode } from '../../../core/exceptions/error-code';
 import { IdeaSubjectRepository } from '../../repository/idea-subject.repository';
 import { TeamRepository } from '../../../team/repository/team.repository';
+import { ApplyRepository } from '../../repository/apply.repository';
+import { SystemSettingRepository } from '../../../system-setting/repository/system-setting.repository';
+import { EApplyStatus } from '../../../core/enums/apply-status.enum';
 
 @Injectable()
 @UseFilters(HttpExceptionFilter)
 export class UpdateIdeaService {
   constructor(
+    private readonly systemSettingRepository: SystemSettingRepository,
     private readonly ideaRepository: IdeaRepository,
     private readonly ideaSubjectRepository: IdeaSubjectRepository,
     private readonly memberRepository: MemberRepository,
     private readonly teamRepository: TeamRepository,
+    private readonly applyRepository: ApplyRepository,
     private readonly dataSource: DataSource
   ) {}
 
   async execute(userId: number, ideaId: number, requestDto: UpdateIdeaRequestDto) : Promise<void> {
     return this.dataSource.transaction(async (manager) => {
+
+      // 시스템 설정 조회
+      const systemSetting = await this.systemSettingRepository.findFirst(manager);
+      if (!systemSetting) {
+        throw new CommonException(ErrorCode.NOT_FOUND_SYSTEM_SETTING);
+      }
+
+      // 현재 phase 조회
+      const currentPhase = systemSetting.getWhichPhase();
 
       // 아이디어 조회
       const idea = await this.ideaRepository.findById(ideaId, manager);
@@ -61,13 +75,17 @@ export class UpdateIdeaService {
         throw new CommonException(ErrorCode.NOT_FOUND_TEAM);
       }
 
+      // 팀의 멤버 조회
       const members = await this.memberRepository.findByIdeaId(requestDto.ideaInfo.ideaSubjectId, manager);
 
-      // 현재 멤버들로 이루어진 특정 파트의 멤버 수 보다 더 적은 수로 파트 정원을 변경하려고 할 시 예외 처리
-      if (requestDto.requirements.pm.capacity < members.filter(member => member.role === 'PM').length ||
-          requestDto.requirements.pd.capacity < members.filter(member => member.role === 'PD').length ||
-          requestDto.requirements.fe.capacity < members.filter(member => member.role === 'FE').length ||
-          requestDto.requirements.be.capacity < members.filter(member => member.role === 'BE').length) {
+      // 해당 아이디어에 대한 지원 정보 조회
+      const applies = await this.applyRepository.findByIdeaIdAndPhase(ideaId, currentPhase, manager);
+
+      // 현재 멤버들로 이루어진 특정 파트의 멤버 수 + 현재 수락한 특정 파트의 지원 수 보다 더 적은 수로 파트 정원을 변경하려고 할 시 예외 처리
+      if (requestDto.requirements.pm.capacity < members.filter(member => member.role === 'PM').length + applies.filter(apply => apply.role === 'PM' && apply.status === EApplyStatus.ACCEPTED).length ||
+          requestDto.requirements.pd.capacity < members.filter(member => member.role === 'PD').length + applies.filter(apply => apply.role === 'PD' && apply.status === EApplyStatus.ACCEPTED).length ||
+          requestDto.requirements.fe.capacity < members.filter(member => member.role === 'FE').length + applies.filter(apply => apply.role === 'FE' && apply.status === EApplyStatus.ACCEPTED).length ||
+          requestDto.requirements.be.capacity < members.filter(member => member.role === 'BE').length + applies.filter(apply => apply.role === 'BE' && apply.status === EApplyStatus.ACCEPTED).length) {
         throw new CommonException(ErrorCode.TEAM_ROLE_CAPACITY_CONFLICT);
       }
 
