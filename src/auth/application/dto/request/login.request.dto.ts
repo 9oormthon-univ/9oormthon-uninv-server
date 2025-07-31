cb913b81f878e0b5d3b156966d08a13fe8520100
장동겸 <jangelliot0404@dgu.ac.kr>
2025-08-01 00:12:51 +0900

    🔨 Fix - RequestDto에 속한 인자들 범위 설정 (#347)

--- src/auth/application/dto/request/login.request.dto.ts
+++ src/auth/application/dto/request/login.request.dto.ts
@@ -10,7 +10,7 @@ export class LoginRequestDto {
   @IsNotEmpty({ message: '아이디를 입력해주세요.' })
   @IsString()
   @Expose({ name: 'serial_id' })
-  @Length(6, 20, { message: '아이디는 6자 이상 20자 이하로 입력해주세요.' })
+  @Length(6, 255, { message: '아이디는 6자 이상 255자 이하로 입력해주세요.' })
   serialId: string;
 
   @ApiProperty({
